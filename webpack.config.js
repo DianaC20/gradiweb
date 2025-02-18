a5eaaae058262a8d1f2e4410d147c93400b9d9ce
Diana Carolina <dcaguirrea@gmail.com>
2025-02-18 13:40:22 -0500

    Asset configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,28 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: ['./src/app.js', 'assets/scss/main.scss'], // Entradas para JS y SCSS
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'main.js',
+    path: path.resolve(__dirname, 'public'), // Carpeta de salida
+    filename: 'main.js', // Nombre del archivo JS de salida
   },
   module: {
     rules: [
       {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /\.scss$/, // Regla para archivos SCSS
+        use: [
+          MiniCssExtractPlugin.loader, // Extrae el CSS en un archivo separado
+          'css-loader', // Interpreta `@import` y `url()`
+          'sass-loader', // Compila SCSS a CSS
+        ],
       },
     ],
   },
-  mode: 'development',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'assets/scss/styles.css', // Ruta y nombre del archivo CSS de salida
+    }),
+  ],
+  mode: 'development', // Modo de desarrollo
 };
